01853c7fa91fd910a9ad2f12b5a685d9e3396e97
marishi <taichi.yanagisaw@gmail.com>
2013-10-19 22:01:00 +0900

    大貫通時のダメージ計算を追加しました。
    formula.pyに第貫通時のダメージ計算式を追加しました。
    damage.htmlに大貫通時の計算式を記述しました。

--- src/formula.py
+++ src/formula.py
@@ -1,17 +1,25 @@
 
 #coding=utf-8
 
+import math
+
 def damage(attack,defense):
 	if attack <= defense:
-		defe = int(defense / 500.0 + 0.9)
-
-		dam = attack * 0.25 - ( (attack*0.01) * defe ) 
+		
+		dam = attack * ( 0.25 - (0.01 * math.ceil( defense / 500.0 ) ) )
 		return ('跳弾',int(dam))
 
 	elif defense < attack and attack < defense*1.3:
 		return ('小貫通','未実装')
 	else:
-		return ('大貫通','未実装')
+		cutdown = 0.8 - ( 0.01 *  math.ceil(defense*0.8/50.0) )
+
+		if cutdown < 0.1:
+			print("0.1")
+			cutdown = 0.1
+
+		dam = attack * cutdown
+		return ('大貫通',int(dam))
 
 def lockon_range(fcs_range,camera):
 	return fcs_range * ((camera + 500.0)/1000.0)
