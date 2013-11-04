434be7278fe5d95d1c6bc1e59a6ec42741b7fd89
marishi <taichi.yanagisaw@gmail.com>
2013-11-04 18:50:24 +0900

    urlの末尾のhtmlを削除しました。

--- src/scripts/apphandler.py
+++ src/scripts/apphandler.py
@@ -6,13 +6,12 @@ import set_dummy_area
 app = webapp2.WSGIApplication(
 	[
 	('/',page.MainPage),
-	('/impact_threshold_acv.html',page.ImpactThresholdAcvPage), 
-	('/lockon_range.html',page.LockonRangePage),
-	('/damage.html',page.DamagePage),
-	('/dps.html',page.DPSPage),
-	('/penetration.html',page.PenetrationPage),
-	('/predict_end_of_war.html',page.PredictEndOfWarPage),
+	('/impact_threshold_acv',page.ImpactThresholdAcvPage), 
+	('/lockon_range',page.LockonRangePage),
+	('/damage',page.DamagePage),
+	('/dps',page.DPSPage),
+	('/penetration',page.PenetrationPage),
+	('/predict_end_of_war',page.PredictEndOfWarPage),
 	('/area_updater', area_updater.AreaUpdater),
-	('/set_dummy_area', set_dummy_area.SetDummyArea),
-	('/world_information.html', page.WorldInformationPage)
+	('/world_information', page.WorldInformationPage)
 	],debug=True)
